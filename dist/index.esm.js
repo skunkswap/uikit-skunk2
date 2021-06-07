26959289d893e835e261ea5dbd3963c5f425f7a7
skunkswap <85455810+skunkswap@users.noreply.github.com>
2021-06-08 01:31:29 +0200

    Create index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2362,7 +2362,7 @@ var PanelFooter = function (_a) {
                                 
                                 
                                 
-          React.createElement(Logo, { width: max-content, mr: "8px" }),
+          React.createElement(Logo, { width: 240, mr: "8px" }),
                                       
                                 
                                 
