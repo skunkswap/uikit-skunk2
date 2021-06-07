6278edf8d4d45ddaa9c21657230d0be3dfb0b4fe
skunkswap <85455810+skunkswap@users.noreply.github.com>
2021-06-08 00:37:56 +0200

    Create index.esm.js

--- dist/index.esm.js
+++ dist/index.esm.js
@@ -2358,7 +2358,7 @@ var PanelFooter = function (_a) {
                                 
          
         React.createElement(SocialEntry, null,
-            cakePriceUsd ? (React.createElement(PriceLink, { href: priceLink, target: "_blank" },
+             (React.createElement(PriceLink, { href: priceLink, target: "_blank" },
                 React.createElement(Icon$v, { width: "24px", mr: "8px" }),
             ))),                    
                                 
